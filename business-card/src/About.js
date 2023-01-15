e3dc11296cc0279bab3bf643764eab74fe21e48a
Adarsh Dalvi <adarshdalvi197@.com>
2023-01-15 19:21:57 +0530

    added Business Card

---
+++ business-card/src/About.js
@@ -0,0 +1,12 @@
+import "./styles/about.css"
+
+export default function About(){
+    return (
+        <main>
+            <h4 className="about" >About</h4>
+            <p className="aboutDesc">I am a frontend developer with a particular interest in making things simple and automating daily &nbsp; tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <h4 className="interests" >Interests</h4>
+            <p className="interestsDesc">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </main>
+    )
+}
